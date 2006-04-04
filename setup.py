4f9b7a46ee92ad96ac8d8e6ab5ad7763fddf424b
Tres Seaver <tseaver@palladion.com>
2006-04-04 19:11:03 +0000

    Package zope.i18nmessageid-3.2.0 as egg.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# Copyright (c) 2004 Zope Corporation and Contributors.
+# Copyright (c) 2006 Zope Corporation and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
@@ -26,7 +26,7 @@ except ImportError, e:
     from distutils.core import setup, Extension
 
 setup(name='zope.i18nmessageid',
-      version='3.0',
+      version='3.2.0',
 
       url='http://svn.zope.org/zope.i18nmessageid',
       license='ZPL 2.1',
