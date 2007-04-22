76709fb288bef4cc6c8857e497b420008336b501
Christian Theune <ct@gocept.com>
2007-04-22 11:02:52 +0000

    Updating version.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ import os
 from setuptools import setup, find_packages, Extension
 
 setup(name='zope.i18nmessageid',
-      version='3.4dev',
+      version = '3.4.0a1',
 
       url='http://svn.zope.org/zope.i18nmessageid',
       license='ZPL 2.1',
