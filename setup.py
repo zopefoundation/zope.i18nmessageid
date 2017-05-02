9632d5a792405cd01fda4b81c518ae84af0d53ba
Tres Seaver <tseaver@palladion.com>
2017-05-02 07:21:44 -0400

    svb

--- setup.py
+++ setup.py
@@ -95,7 +95,7 @@ class optional_build_ext(build_ext):
         sys.stderr.write('*' * 80 + '\n')
 
 setup(name='zope.i18nmessageid',
-    version='4.1.0',
+    version='4.1.1.dev0',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
     description='Message Identifiers for internationalization',
