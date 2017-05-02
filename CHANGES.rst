01562fdf0cb808a408b947416d430e359342f6e7
Tres Seaver <tseaver@palladion.com>
2017-05-02 07:12:22 -0400

    Prep 4.1.0 release.

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changes
 =======
 
-4.1.0 (unreleased)
+4.1.0 (2017-05-02)
 ------------------
 
 - Drop support for Python 2.6 and 3.2.
--- setup.py
+++ setup.py
@@ -95,7 +95,7 @@ class optional_build_ext(build_ext):
         sys.stderr.write('*' * 80 + '\n')
 
 setup(name='zope.i18nmessageid',
-    version='4.1.0.dev0',
+    version='4.1.0',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
     description='Message Identifiers for internationalization',
