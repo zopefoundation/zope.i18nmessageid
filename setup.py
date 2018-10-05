ae338be03256136e684b4d5fee2b3b9dccaa6f2b
Michael Howitz <mh@gocept.com>
2018-10-05 18:14:40 +0200

    Back to development: 4.3

--- setup.py
+++ setup.py
@@ -100,7 +100,7 @@ class optional_build_ext(build_ext):
 
 setup(
     name='zope.i18nmessageid',
-    version='4.2',
+    version='4.3.dev0',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
     description='Message Identifiers for internationalization',
