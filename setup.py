9ad13169c310b7a3a5ec8c5368bc5bfcc880f8b2
Marius Gedminas <marius@gedmin.as>
2020-03-10 14:38:49 +0200

    Back to development: 5.0.2

--- setup.py
+++ setup.py
@@ -94,7 +94,7 @@ class optional_build_ext(build_ext):
 
 setup(
     name='zope.i18nmessageid',
-    version='5.0.1',
+    version='5.0.2.dev0',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
     description='Message Identifiers for internationalization',
