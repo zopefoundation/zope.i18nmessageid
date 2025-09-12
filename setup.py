e4ba1636e13082d9bbdd8cb5c7ce9bf2a7445bb3
Michael Howitz <icemac@gmx.net>
2025-09-12 09:16:02 +0200

    Back to development: 8.1

--- setup.py
+++ setup.py
@@ -101,7 +101,7 @@ class optional_build_ext(build_ext):
 
 setup(
     name='zope.i18nmessageid',
-    version='8.0',
+    version='8.1.dev0',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.dev',
     description='Message Identifiers for internationalization',
