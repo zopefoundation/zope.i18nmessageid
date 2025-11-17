fe2abc3c7994bc3de5b7665a6b48f0954a54e889
Jens Vagelpohl <jens@plyp.com>
2025-11-17 07:31:38 +0100

    - move metadata into ``pyproject.toml``, reformat change log

--- setup.py
+++ setup.py
@@ -58,17 +58,6 @@ if not is_pypy and not is_jython:
     ext_modules = codeoptimization
 
 
-tests_require = [
-    'zope.testrunner >= 6.4',
-    'coverage',
-]
-
-
-def read(*rnames):
-    with open(os.path.join(os.path.dirname(__file__), *rnames)) as stream:
-        return stream.read()
-
-
 class optional_build_ext(build_ext):
     """This class subclasses build_ext and allows
        the building of C extensions to fail.
@@ -99,51 +88,5 @@ class optional_build_ext(build_ext):
         print('*' * 80, file=sys.stderr)
 
 
-setup(
-    name='zope.i18nmessageid',
-    version='8.2.dev0',
-    author='Zope Foundation and Contributors',
-    author_email='zope-dev@zope.dev',
-    description='Message Identifiers for internationalization',
-    long_description=(
-        read('README.rst')
-        + '\n\n' +
-        read('CHANGES.rst')
-    ),
-    keywords="zope i18n message factory",
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Zope Public License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-        'Programming Language :: Python :: 3.13',
-        'Programming Language :: Python :: 3.14',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Topic :: Internet :: WWW/HTTP',
-        'Framework :: Zope :: 3',
-    ],
-    license='ZPL-2.1',
-    url='https://github.com/zopefoundation/zope.i18nmessageid',
-    # we need the following two parameters because we compile C code,
-    # otherwise only the shared library is installed:
-    package_dir={'': 'src'},
-    packages=['zope.i18nmessageid'],
-    python_requires='>=3.10',
-    include_package_data=True,
-    zip_safe=False,
-    cmdclass={'build_ext': optional_build_ext},
-    ext_modules=ext_modules,
-    extras_require={
-        'testing': tests_require,
-        'test': tests_require,
-        'docs': ['Sphinx', 'sphinx_rtd_theme'],
-    },
-)
+setup(cmdclass={'build_ext': optional_build_ext},
+      ext_modules=ext_modules)
